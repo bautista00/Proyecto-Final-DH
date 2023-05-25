e8a5eaf295c54b4f1e7b337ee8eb8d4f53004a3b
bautista00 <bautistaquesada00@gmail.com>
2023-05-25 17:07:04 -0300

    configuracion del AWS S3 bucket de img

---
+++ Back-End/backend-pi/src/main/java/com/example/backendpi/controller/UploadFileController.java
@@ -0,0 +1,42 @@
+package com.example.backendpi.controller;
+
+import com.example.backendpi.service.api.AwsS3Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/S3")
+public class UploadFileController {
+
+
+    @Autowired
+    private AwsS3Service awss3Service;
+
+    @PostMapping(value = "/upload")
+    public ResponseEntity<String> uploadFile(@RequestPart(value="file") MultipartFile file) {
+        awss3Service.uploadFile(file);
+        String response = "El archivo "+file.getOriginalFilename()+" fue subido exitosamente al bucket S3";
+        return new ResponseEntity<String>(response, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/list")
+    public ResponseEntity<List<String>> listFiles() {
+        return new ResponseEntity<List<String>>(awss3Service.getObjectsFromS3(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/download")
+    public ResponseEntity<Resource> download(@RequestParam("key") String key) {
+        InputStreamResource resource  = new InputStreamResource(awss3Service.downloadFile(key));
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+key+"\"").body(resource);
+    }
+
+}
---
+++ Back-End/backend-pi/src/main/java/com/example/backendpi/service/AwsS3ServiceImpl.java
@@ -0,0 +1,65 @@
+package com.example.backendpi.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.example.backendpi.service.api.AwsS3Service;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+public class AwsS3ServiceImpl implements AwsS3Service {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AwsS3ServiceImpl.class);
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    @Override
+    public void uploadFile(MultipartFile file) {
+        File mainFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        try (FileOutputStream stream = new FileOutputStream(mainFile)) {
+            stream.write(file.getBytes());
+            String newFileName = System.currentTimeMillis() + "_" + mainFile.getName();
+            LOGGER.info("Se sube archivo con el nombre... " + newFileName);
+            PutObjectRequest request = new PutObjectRequest(bucketName, newFileName, mainFile);
+            amazonS3.putObject(request);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public List<String> getObjectsFromS3() {
+        ListObjectsV2Result result = amazonS3.listObjectsV2(bucketName);
+        List<S3ObjectSummary> objects = result.getObjectSummaries();
+        return objects.stream().map(S3ObjectSummary::getKey).collect(Collectors.toList());
+    }
+
+    @Override
+    public InputStream downloadFile(String key) {
+        S3Object object = amazonS3.getObject(bucketName, key);
+        return object.getObjectContent();
+    }
+
+}
