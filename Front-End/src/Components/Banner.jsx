f314595cefd7b494e9892cbc16c0b60200d27cd0
Estefania Escarria <estefy0114@gmail.com>
2023-06-13 13:20:42 -0500

    Se elimina carpeta de front

--- Front-End/src/Components/Banner.jsx
+++
@@ -1,9 +0,0 @@
-export const Banner = ({ title }) => {
-  return (
-    <div className="banner-container">
-      <div className="banner-text">{title}</div>
-    </div>
-  );
-};
-
-export default Banner;
