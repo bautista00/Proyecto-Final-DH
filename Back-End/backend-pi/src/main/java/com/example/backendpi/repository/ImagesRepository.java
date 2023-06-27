95701e34e7b95014f3256d6d1010af8dc80f893e
LorenzoGazza <lorenzogazzani00@gmail.com>
2023-06-27 17:00:42 -0300

    Eliminar cat

--- Back-End/backend-pi/src/main/java/com/example/backendpi/repository/ImagesRepository.java
+++ Back-End/backend-pi/src/main/java/com/example/backendpi/repository/ImagesRepository.java
@@ -1,7 +1,11 @@
 package com.example.backendpi.repository;
 
+import com.example.backendpi.domain.Categoria;
 import com.example.backendpi.domain.Images;
+import com.example.backendpi.exceptions.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImagesRepository extends JpaRepository<Images,Long> {
+
+    Images findByCategoria(Categoria categoria) throws ResourceNotFoundException;
 }
